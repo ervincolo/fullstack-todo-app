d00d02561af0bd8d996b2be6c35de0d7ac386c5c
Ervin Colo <79028899+ervincolo@users.noreply.github.com>
2025-11-02 19:41:47 +0100

    Save button added, patch route (backend) added

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,41 +1,67 @@
 //ovdje idu svi api (endpoints za taskove)!
 
-import express, { Router } from 'express';
-import Task from '../models/Task.js';
+import express, { Router } from "express";
+import Task from "../models/Task.js";
 
 const router = express.Router();
 
 // uzmi sve tasks ruta
 // GET all tasks
-router.get('/', async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.json(tasks);
-    } catch (err) {
-        res.status(500).json({message: err.message});
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// edituj novi task
+// PATCH new task
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const { title } = req.body;
+    if (!title || title.trim().length === 0) {
+      return res.status(400).json({ message: "Title should not be empty.." });
     }
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      { title: title },
+      { new: true }
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // dodaj novi task ruta
 // POST new task
-router.post('/', async (req, res) => {
-    try {
-        const newTask = await Task.create({title: req.body.title});
-        res.status(201).json(newTask);
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
+router.post("/", async (req, res) => {
+  try {
+    const newTask = await Task.create({ title: req.body.title });
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // izbrisi task ruta
 // DELTETE task
-router.delete('/:id', async (req, res) => {
-    try {
-        await Task.findByIdAndDelete(req.params.id);
-        res.json({message: 'Task deleted'});
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
+router.delete("/:id", async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: "Task deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import React, {use, useEffect, useState} from 'react';
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 // tasks, drzi listu svih taskova koje si dodao ili ucitao.
 // newTask drzi vrijednost iz input polja.
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
-  const [editedTask, setNewEditTask] = useState('');
+  const [newTask, setNewTask] = useState("");
+  const [editedCurrentTask, setNewEditTask] = useState("");
   const [editingId, setEditingId] = useState(null);
 
   // useEffect(() => {
@@ -17,125 +17,146 @@ function App() {
   // }, []);
 
   //drugi nacin => second and more understandable way
-//   useEffect(() => {
-//     const fetchTasks = async () => {
-//       try {
-//         const res = await fetch("http://localhost:4000/tasks");
-//         const data = await res.json();
-//         setTasks(data);
-//       } catch (err) {
-//         console.err(err);
-//       }
-//   };
-//   fetchTasks();
-// }, []);
+  //   useEffect(() => {
+  //     const fetchTasks = async () => {
+  //       try {
+  //         const res = await fetch("http://localhost:4000/tasks");
+  //         const data = await res.json();
+  //         setTasks(data);
+  //       } catch (err) {
+  //         console.err(err);
+  //       }
+  //   };
+  //   fetchTasks();
+  // }, []);
 
+  // Adding The Task.
+  const handleAddTask = async () => {
+    if (!newTask) return; //do not do anything if empty (ako je prazno ne radi nista) tj falsy value.
 
-// Adding The Task.
-const handleAddTask = async () => {
-    if (!newTask) 
-    return; //do not do anything if empty (ako je prazno ne radi nista) tj falsy value.
+    try {
+      const res = await fetch("http://localhost:4000/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: newTask }),
+      });
+      const data = await res.json(); //backend vraca JSON sa novim tasks.
+      console.log(data); //samo da testiram da vidim sta vraca backend.
+      setTasks([...tasks, data]); //dodaje novi task u state i update-uje UI.
+      setNewTask("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  // Deleting The Added Task.
+  const handleDltTask = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:4000/tasks/${id}`, {
+        method: "DELETE",
+        //headers takodje nije potreban jer se on koristi samo kad saljes podatke u body-u (POST, PATCH).
+        //body nije potreban u ovom slucaju jer kada brisemo neki resurs ne saljemo nikakav body.
+        //backend vec zna koji task da obrise iz URL-a (/tasks/${id}).
+      });
+      const data = await res.json();
+      console.log(data);
+      setTasks(tasks.filter((task) => task._id !== id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  try {
-    const res = await fetch("http://localhost:4000/tasks", {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({title: newTask})
-  });
-    const data = await res.json(); //backend vraca JSON sa novim tasks.
-    console.log(data); //samo da testiram da vidim sta vraca backend.
-    setTasks([...tasks, data]); //dodaje novi task u state i update-uje UI.
-    setNewTask('');
-  } catch (err) {
-    console.error(err);
-  }
-};
-// Deleting The Added Task.
-const handleDltTask = async (id) => {
-  try {
-    const res = await fetch(`http://localhost:4000/tasks/${id}`, {
-        method: 'DELETE',
-      //headers takodje nije potreban jer se on koristi samo kad saljes podatke u body-u (POST, PATCH).
-      //body nije potreban u ovom slucaju jer kada brisemo neki resurs ne saljemo nikakav body.
-      //backend vec zna koji task da obrise iz URL-a (/tasks/${id}).
-    });
-    const data = await res.json();
-    console.log(data);
-    setTasks(tasks.filter(task => task._id !== id)); //
-  } catch (err)
- {
-  console.error(err);
- }};
-
- // Editing the added task.
- const handleEditTask = async (id) => {
-
-  try {
-    const res = await fetch(`http://localhost:4000/tasks/${id}`, {
-      method: 'PATCH',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({title: editedTask})
-    });
-    const data = await res.json();
-    console.log(data);
-
-    //here i need to finish editing task function
-    //ovdje zavrsiti edit funkciju
-    
-
-  } catch (err) {
-    console.error(err);
-  }};
+  // Editing the added task.
+  // Edituje task na osnovu ID-a.
+  const handleEditTask = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:4000/tasks/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: editedCurrentTask }),
+      });
+      const data = await res.json();
+      console.log(data);
+      setTasks(tasks.map((task) => (task._id === id ? data : task)));
+      setEditingId(null);
+      setNewEditTask("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   // here (after return) comes everything that you want to see in browser (UI-rendering).
   // ovdje poslije returna dolazi frontend tj klijent strana (UI).
-
-    return (
+  return (
     <>
-     <div className="main-container">
-      <h1>My Tasks</h1>
+      <div className="main-container">
+        <h1>Tasks for Today</h1>
+        <button className="addBtn" onClick={handleAddTask}>
+          ADD
+        </button>
+        <div className="input-tasks">
+          <input
+            type="text"
+            placeholder="What is up.."
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)} //ovo hvata unos taska.
+          />
+          <ul>
+            {tasks.map(
+              (
+                task //vraca novi array od tasks
+              ) => (
+                <li key={task._id}>
+                  {editingId === task._id ? (
+                    <>
+                      <input
+                        type="text"
+                        value={editedCurrentTask}
+                        onChange={(e) => setNewEditTask(e.target.value)}
+                      />
+                      <button
+                        className="saveBtn"
+                        onClick={() => {
+                          if (editedCurrentTask.trim().length === 0) {
+                            console.log("Please enter something..");
+                            return;
+                          }
+                          if (editedCurrentTask === task.title) {
+                            return;
+                          }
+                          handleEditTask(task._id);
+                        }}
+                      >
+                        SAVE
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      {task.title}
+                      <button
+                        className="editBtn"
+                        onClick={() => {
+                          setEditingId(task._id);
+                          setNewEditTask(task.title);
+                        }}
+                      >
+                        EDIT
+                      </button>
 
-  
-      <button className='addBtn' onClick={handleAddTask}>ADD</button>
-      <div className="input-tasks">
-       <input type="text"
-        placeholder='Add New Task..'
-        value= {newTask} 
-        onChange={ e => setNewTask(e.target.value)} //ovo hvata unos taska.
-      />
-
-  
-      <ul>
-        {tasks.map(task => //vraca novi array od tasks
-          <li key={task._id}>
-            {task.title}
-
-          <button className='dltBtn' onClick={() => handleDltTask(task._id)}>
-            DELETE
-          </button>
-
-          <button className='editBtn' onClick={() => setEditingId(task._id)}>
-            EDIT
-          </button>
-
-          {editingId === task._id && (
-            <input 
-            type='text'
-            value={editedTask}
-            onChange={e => setNewEditTask(e.target.value)}
-            />
-          )}
-          </li>
-        )}
-        </ul>
-
-      
-      
-     </div>
-    </div>
-  </>
-);
-
-
-};
+                      <button
+                        className="dltBtn"
+                        onClick={() => handleDltTask(task._id)}
+                      >
+                        DELETE
+                      </button>
+                    </>
+                  )}
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
 export default App;
